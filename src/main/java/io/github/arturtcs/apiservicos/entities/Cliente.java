9639055acdd3bfd7dbcf80dee90b31bf6a262547
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-02-22 18:02:06 -0300

    Criando a entidade Cliente e adicionando projeto lombok ao projeto

---
+++ src/main/java/io/github/arturtcs/apiservicos/entities/Cliente.java
@@ -0,0 +1,26 @@
+package io.github.arturtcs.apiservicos.entities;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Cliente implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    private LocalDate dataCadastro;
+}
