011fb3357cbe2224743038dbcb56088855e35f79
Artur Tarcisio <arturtarcisio1@gmail.com>
2021-02-23 20:01:56 -0300

    Adicionando dependencias do bean e hibernate validation e inserindo algumas validacoes na entidade de Cliente

--- src/main/java/io/github/arturtcs/apiservicos/entities/Cliente.java
+++ src/main/java/io/github/arturtcs/apiservicos/entities/Cliente.java
@@ -2,8 +2,11 @@ package io.github.arturtcs.apiservicos.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 import java.time.LocalDate;
 
@@ -19,9 +22,12 @@ public class Cliente implements Serializable {
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull
+    @CPF
     private String cpf;
 
     @Column(name = "data_cadastro", updatable = false)
